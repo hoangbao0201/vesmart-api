562525ac36441182a5890062350f3e387f9081de
hoangbao0201 <hoangbao020103@gmail.com>
2023-08-09 21:05:48 +0700

    Add Model User, g Auth(Login, Register)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    PrismaModule,
+    AuthModule,
+    PrismaModule, 
   ],
   controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
