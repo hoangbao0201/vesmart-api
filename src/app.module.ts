6b167c621ba4b80cc7be4d3f62645fbfa5f8da57
hoangbao0201 <hoangbao020103@gmail.com>
2023-08-10 00:00:32 +0700

    Blog(Create, GetBySlug, GetAll, Update, Delete)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true
     }),
     AuthModule,
+    BlogModule,
     PrismaModule, 
   ],
   controllers: [AppController],
