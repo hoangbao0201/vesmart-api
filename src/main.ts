7676ea0633f496dd999ca7aa11e9c32e07b3dabd
hoangbao0201 <hoangbao020103@gmail.com>
2023-08-10 10:37:54 +0700

    Fix deploy (Config main.ts)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Add middleware HERE!
   app.useGlobalPipes(new ValidationPipe())
 
